7b4daa4c01eee410a23656f6c19cca91d330417e
Rachel Wyatt <rw126@evansville.edu>
2019-03-03 14:59:25 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def add_to_cart
-    @item = Item.find(params[:id])
-    current_cart << @item.id
-  end
-
   def cart
     session[:cart] ||= []
   end
